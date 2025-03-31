228c4b4abbbf169d9f9486d410900540935930e4
b14nc444 <b14nc4@naver.com>
2025-03-31 23:08:10 +0900

    feat: 색상 상수 추가 - PRIMARY, SECONDARY, TERTIARY 버튼 스타일 정의

---
+++ src/constants/color.js
@@ -0,0 +1,64 @@
+export const PRIMARY = {
+    BUTTON: {
+        DEFAULT: {
+            COLOR: "#fff",
+            BACKGROUND_COLOR: "#6542f1",
+        },
+        HOVER: {
+            COLOR: "#fff",
+            BACKGROUND_COLOR: "#9982f4",
+        },
+        PRESSED: {
+            COLOR: "#fff",
+            BACKGROUND_COLOR: "#4b31b0",
+        },
+        DISABLED: {
+            COLOR: "#d0cdcd",
+            BACKGROUND_COLOR: "#ededed",
+        },
+    },
+};
+export const SECONDARY = {
+    BUTTON: {
+        DEFAULT: {
+            COLOR: "#3a3a3a",
+            BACKGROUND_COLOR: "#dedede",
+        },
+        HOVER: {
+            COLOR: "#3a3a3a",
+            BACKGROUND_COLOR: "#f3f3f3",
+        },
+        PRESSED: {
+            COLOR: "#3a3a3a",
+            BACKGROUND_COLOR: "#b8b7b9",
+        },
+        DISABLED: {
+            COLOR: "#d0cdcd",
+            BACKGROUND_COLOR: "#ededed",
+        },
+    },
+};
+export const TERTIARY = {
+    BUTTON: {
+        DEFAULT: {
+            COLOR: "#6542f1",
+            BACKGROUND_COLOR: "#ffffff",
+            BORDER: "#6542f1",
+        },
+        HOVER: {
+            COLOR: "#9982f4",
+            BACKGROUND_COLOR: "#ffffff",
+            BORDER: "#9982f4",
+        },
+        PRESSED: {
+            COLOR: "#4b31b0",
+            BACKGROUND_COLOR: "#f0edfc",
+            BORDER: "#4b31b0",
+        },
+        DISABLED: {
+            COLOR: "#d0cdcd",
+            BACKGROUND_COLOR: "#f6f6f6",
+            BORDER: "#ededed",
+        },
+    },
+};
