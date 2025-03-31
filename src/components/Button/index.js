a2f2e158b8407adf50af6c4c926cef2c10d6282d
b14nc444 <b14nc4@naver.com>
2025-03-31 23:03:47 +0900

    fix: 버튼 스타일링 문제 수정 중 - TERTIARY 버튼의 border 스타일이 제대로 적용되지 않는 문제 발생

---
+++ src/components/Button/index.js
@@ -0,0 +1,70 @@
+import styled from "styled-components";
+
+import { PRIMARY, SECONDARY, TERTIARY } from "../../constants/color";
+
+const colorMap = {
+    PRIMARY: {
+        DEFAULT: PRIMARY.BUTTON.DEFAULT,
+        HOVER: PRIMARY.BUTTON.HOVER,
+        PRESSED: PRIMARY.BUTTON.PRESSED,
+        DISABLED: PRIMARY.BUTTON.DISABLED,
+    },
+    SECONDARY: {
+        DEFAULT: SECONDARY.BUTTON.DEFAULT,
+        HOVER: SECONDARY.BUTTON.HOVER,
+        PRESSED: SECONDARY.BUTTON.PRESSED,
+        DISABLED: SECONDARY.BUTTON.DISABLED,
+    },
+    TERTIARY: {
+        DEFAULT: TERTIARY.BUTTON.DEFAULT,
+        HOVER: TERTIARY.BUTTON.HOVER,
+        PRESSED: TERTIARY.BUTTON.PRESSED,
+        DISABLED: TERTIARY.BUTTON.DISABLED,
+    },
+};
+
+const Button = styled.button `
+  padding: 16px 24px;
+  width: 200px;
+
+  color: ${({ type }) => colorMap[type].DEFAULT.COLOR};
+  background-color: ${({ type }) => colorMap[type].DEFAULT.BACKGROUND_COLOR};
+  border-radius: 10px;
+  font-weight: bold;
+
+  border: ${({ type }) => {
+    // console.log({ type });
+    // console.log(type === "TERTIARY" ? `
+1 px solid rgb(255, 29, 29)
+` : "none");
+    // console.log(type === "TERTIARY");
+    return `
+1 px solid $ { TERTIARY.BUTTON.DEFAULT.BORDER }
+`;
+  }};
+
+  &:hover {
+    color: ${({ type }) => colorMap[type].HOVER.COLOR};
+    background-color: ${({ type }) => colorMap[type].HOVER.BACKGROUND_COLOR};
+    border: 1px solid ${TERTIARY.BUTTON.HOVER.BORDER};
+  }
+
+  &:active {
+    color: ${({ type }) => colorMap[type].PRESSED.COLOR};
+    background-color: ${({ type }) => colorMap[type].PRESSED.BACKGROUND_COLOR};
+    border: ${({ type }) => (type === "TERTIARY" ? `
+1 px solid $ { colorMap[type].PRESSED.BORDER }
+` : "none")};
+  }
+
+  &:disabled {
+    background-color: ${({ type }) => colorMap[type].DISABLED.BACKGROUND_COLOR};
+    color: ${({ type }) => colorMap[type].DISABLED.COLOR};
+    border: ${({ type }) => (type === "TERTIARY" ? `
+1 px solid $ { colorMap[type].DISABLED.BORDER }
+` : "none")};
+    cursor: not-allowed;
+  }
+`;
+
+export default Button;
